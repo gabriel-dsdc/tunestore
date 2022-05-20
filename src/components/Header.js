54ebde385905371b50395685bfc9cdc33098475b
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2022-05-20 18:05:18 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
+import Logo from '../assets/logo_mini.png';
+import Avatar from '../assets/avatar.png';
 
 class Header extends React.Component {
   state = {
@@ -17,7 +20,22 @@ class Header extends React.Component {
     const { username, isLoading } = this.state;
     return (
       <header data-testid="header-component">
-        <h1 data-testid="header-user-name">{isLoading ? <Loading /> : username}</h1>
+        <div className="header-content">
+          <div className="header-logo">
+            <img src={ Logo } alt="Logo da TrybeTunes" />
+          </div>
+          <div className="user-container">
+            <img src={ Avatar } alt="Imagem de perfil" />
+            <h1 data-testid="header-user-name" className="user-name-paragraph">
+              {isLoading ? <Loading /> : username}
+            </h1>
+          </div>
+          <div className="header-links">
+            <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
+            <Link data-testid="link-to-favorites" to="/favorites">Favoritos</Link>
+            <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+          </div>
+        </div>
       </header>);
   }
 }
