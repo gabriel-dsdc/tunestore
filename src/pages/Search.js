07586c0a5a6334bbd1317ebee55222b8d21e9f76
Gabriel dos Santos Duarte Carvalhães <gabriel.ds.duarte99@gmail.com>
2022-05-20 19:53:25 -0300

    Requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,10 +2,39 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchInput: '',
+  }
+
   render() {
+    const { searchInput } = this.state;
+    const isSearchDisabled = () => {
+      if (searchInput.length >= 2) {
+        return false;
+      }
+      return true;
+    };
     return (
       <div data-testid="page-search">
         <Header />
+        <div className="search-input-content">
+          <input
+            data-testid="search-artist-input"
+            type="text"
+            className="input-area"
+            name="searchInput"
+            value={ searchInput }
+            onChange={ ({ target: { name, value } }) => this.setState({ [name]: value }) }
+          />
+          <button
+            data-testid="search-artist-button"
+            type="submit"
+            className="search-button"
+            disabled={ isSearchDisabled() }
+          >
+            Pesquisar
+          </button>
+        </div>
       </div>
     );
   }
